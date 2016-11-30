29551d785041de9c56909bac4711d30b03ef2c02
Jeremy Cummins <jeremy.z.cummins@gmail.com>
2016-11-30 13:22:25 -0800

    Handle Asana exports with different columns (some projects have additional fields)

--- process.py
+++ process.py
@@ -1,5 +1,20 @@
 import csv
+import sys
+import os
 from datetime import datetime
+
+if len(sys.argv) < 2:
+    print 'This script requires an input filename and optional output filename (defaults to "output.csv")'
+    exit()
+
+inputfile = str(sys.argv[1])
+outputfile = str(sys.argv[2]) if len(sys.argv) > 2 else 'output.csv'
+
+
+try:
+    os.remove(outputfile)
+except OSError:
+    pass
 
 
 def change_date(date_str):
@@ -8,7 +23,7 @@ def change_date(date_str):
     return datetime.strptime(date_str, "%Y-%m-%d").strftime("%b %d, %Y")
 
 
-with open('output.csv', 'a+') as csvfileout:
+with open(outputfile, 'a+') as csvfileout:
     writer = csv.writer(csvfileout, delimiter=',',
                         quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
@@ -17,14 +32,20 @@ with open('output.csv', 'a+') as csvfileout:
                      'Owned By'])
 
     '''
-    Asana headers to map:
+    Pivotal headers:
+
+    [0'Id', 1'Title', 2'Labels', 3'Iteration', 4'Iteration Start', 5'Iteration End', 6'Type', 7'Estimate',
+     8'Current State', 9'Created at', 10'Accepted at', 11'Deadline', 12'Requested By', 13'Description', 14'URL',
+     15'Owned By']
+
+    Asana headers:
 
     [0'Task ID', 1'Created At', 2'Completed At', 3'Last Modified', 4'Name', 5'Assignee', 6'Due Date', 7'Tags', 8'Notes',
      9'Projects', 10'Parent Task', 11'Steps to Reproduce', 12'Client Name/ ID', 13'Expected Behavior', 14'Urgent?',
      15'URL of Widget']
     '''
 
-    with open('Sprint_15_Juster.csv', 'rb') as csvfilein:
+    with open(inputfile, 'rb') as csvfilein:
         reader = csv.reader(csvfilein, delimiter=',', quotechar='"')
         is_first = True
         for row in reader:
@@ -34,22 +55,44 @@ with open('output.csv', 'a+') as csvfileout:
 
             created_at = change_date(row[1])
             accepted_at = change_date(row[2])
-            deadline = change_date(row[6])
+
+            if row[6] == '':
+                deadline = ''
+            elif datetime.now() > datetime.strptime(row[6], "%Y-%m-%d"):
+                print 'Past deadline found, removing deadline from task: ' + row[4]
+                deadline = ''
+            else:
+                deadline = change_date(row[6])
+
             description = row[8]
 
             if accepted_at != '':
                 status = 'accepted'
-                estimate = 1
+                estimate = 3
             else:
                 status = 'unscheduled'
                 estimate = ''
 
-            if row[11] != '':
-                if description != '':
-                    description += '\n\n'
-                description += row[11]
+            try:
+                if row[11] != '':
+                    if description != '':
+                        description += '\n\n'
+                    description += row[11]
+            except IndexError:
+                pass
+
+            try:
+                requested_by = row[12]
+            except IndexError:
+                requested_by = ''
+
+            try:
+                owned_by = row[15]
+            except IndexError:
+                owned_by = ''
+
             mapped_row = [row[0], row[4], row[7], '', '', '', 'feature', estimate, status, created_at, accepted_at,
-                         deadline, row[12], description, row[15], row[5]]
-            print(mapped_row)
+                          deadline, requested_by, description, owned_by, row[5]]
+            # print(mapped_row)
             writer.writerow(mapped_row)
 
